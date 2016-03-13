a581e531e683d1849abeb648038bc7e038699e4e
law-liet <lawliet@orion.codes>
2016-03-13 18:33:38 -0400

    [Add] more stuff

--- Main/urls.py
+++ Main/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='home')
+    url(r'^$', views.index, name='home'),
+    url(r'^register/', views.register, name='register'),
+    url(r'^panelist/', views.panelist, name='panelist'),
+    url(r'^feature_events', views.feature_events, name='feature_events')
 ]
--- Main/views.py
+++ Main/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'Main/index.html')
+
+def register(request):
+    return render(request, 'Main/register.html')
+
+def panelist(request):
+    return render(request, 'Main/panelist.html')
+
+def feature_events(request, event = 'lunch-event'):
+    return render(request, 'Main/featured_events.html', {'event' : event})
