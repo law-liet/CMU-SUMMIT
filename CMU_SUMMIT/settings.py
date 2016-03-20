c1015620531cf8a3e0819099efc9637ccd07bbbe
law-liet <lawliet@orion.codes>
2016-03-19 23:41:19 -0400

    [Modify] models

--- CMU_SUMMIT/settings.py
+++ CMU_SUMMIT/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'gc@jbelo9u(lc%f&+5#qyaxf8_kl39ap$zxx*)p&mr+suip+j_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
--- Main/models.py
+++ Main/models.py
@@ -2,7 +2,19 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=64)
-    email = models.EmailField(max_length=75)
-    phone = models.CharField(max_length= 13)
+    email = models.EmailField(max_length=64)
+    phone = models.CharField(max_length= 64)
     role = models.CharField(max_length=64)
-    interested_company = models.CharField(max_length= 128)
+    degree = models.CharField(max_length=64, default='none')
+    major = models.CharField(max_length=64, default='none')
+    department = models.CharField(max_length=128, default='none')
+    institution = models.CharField(max_length=128, default='none')
+
+class Applicant(models.Model):
+    name = models.CharField(max_length=64)
+    email = models.EmailField(max_length=64)
+    major = models.CharField(max_length=64)
+    university = models.CharField(max_length=128)
+    graduate_time = models.DateField()
+    interested_company = models.CharField(max_length=128)
+    resume = models.FileField(upload_to='', default=None)
